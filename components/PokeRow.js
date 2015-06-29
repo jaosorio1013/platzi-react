c48dba43515767a99f1f073f9b38b3fe37a8b935
jaosorio1013 <jaosorio1013@gmail.com>
2015-06-29 00:44:10 -0500

    modularizando la aplicacion

---
+++ components/PokeRow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PokeAvatar from './PokeAvatar'
+
+export default class PokeRow extends React.Component {
+    onClick(ev) {
+        alert(this.props.name);
+    }
+
+    render() {
+        return <li className="pokerow" onClick={this.onClick.bind(this)}>
+            <PokeAvatar number={this.props.number} />
+            {this.props.name}
+        </li>
+    }
+}
